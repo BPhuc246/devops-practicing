6788191f9a47b88abdb71197deb5e1a5f854d08d
Phúc <nhbphuc2006@gmail.com>
2026-06-26 08:14:14 +0700

    phase-5: fix filter function error

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 });
 
 initDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
   });
 });
