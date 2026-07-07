87d6a55f023326caf5d94e32128b9c8fccab064b
Phúc <nhbphuc2006@gmail.com>
2026-07-07 14:35:28 +0700

    phase_5: update code

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true,
-    allowedHosts: ["app.local"],
+    allowedHosts: ["tododevops.duckdns.org"],
+    proxy: {
+      "/api": {
+        target: "http://backend:8080",
+        changeOrigin: true,
+      },
+    },
   },
 });
